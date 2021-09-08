0a258056bcbea28454de253b1566accee690b80c
Alexander Williams <alexander.williams@knights.ucf.edu>
2021-09-07 23:29:16 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args ) {
+        System.out.print("What is the input string? ");
+
+        Scanner scan = new Scanner(System.in);
+        String input_string = scan.nextLine();
+
+        int string_length = input_string.length();
+
+        System.out.println(input_string + " has " + string_length + " characters.");
+    }
+}
